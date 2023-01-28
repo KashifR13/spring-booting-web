68b379be7790d340f48546beb4e56e083c7c30f3
Kashif Rasool <kashif.rasool@tuni.fi>
2023-01-28 23:56:42 +0200

    Initial commit

---
+++ src/main/java/spring/fi/springbootingweb/SpringBootingWebApplication.java
@@ -0,0 +1,13 @@
+package spring.fi.springbootingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootingWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootingWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/fi/springbootingweb/SpringBootingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.fi.springbootingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootingWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
