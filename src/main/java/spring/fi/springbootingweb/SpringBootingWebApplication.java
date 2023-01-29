4a2c234f57b2de7d8d0e080ce2e8e5e2eb1bf3e1
Kashif Rasool <kashif.rasool@tuni.fi>
2023-01-29 18:21:44 +0200

    Build FizzBuzz CLR application

--- src/main/java/spring/fi/springbootingweb/SpringBootingWebApplication.java
+++ src/main/java/spring/fi/springbootingweb/SpringBootingWebApplication.java
@@ -44,6 +44,30 @@ public class SpringBootingWebApplication {
 		};
 	}
 
+	@Bean
+	public CommandLineRunner fizzBuzzCLRChallenge (RestTemplate restTemplate)
+			throws Exception {
+		return args -> {
+
+			LOG.info("Starting FIZZBUZZ CLR application...");
+
+			for (int i=1; i<=100; i++) {
+
+				if (i%3 == 0 && i%5 ==0)
+					LOG.info("FizzBuzz");
+				else if (i%3 == 0)
+					LOG.info("Fizz");
+				else if (i%5 == 0)
+					LOG.info("Buzz");
+				else
+					LOG.info(i+" ");
+			}
+
+			LOG.info("Terminating FIZZBUZZ CLR application...");
+		};
+	}
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootingWebApplication.class, args);
 	}
