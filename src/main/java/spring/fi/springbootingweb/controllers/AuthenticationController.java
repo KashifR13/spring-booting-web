86793a71173974f1cc3ce94e640fb1c7df2e4f81
Kashif Rasool <kashif.rasool@tuni.fi>
2023-02-03 18:36:18 +0200

    Add SpringSecurity by extending 'WebSecurityConfigurerAdapter' and using '@EnableWebSecurity'

---
+++ src/main/java/spring/fi/springbootingweb/controllers/AuthenticationController.java
@@ -0,0 +1,13 @@
+package spring.fi.springbootingweb.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthenticationController {
+
+    @GetMapping("/login")
+    public String getLogin(){
+        return "login";
+    }
+}
